dfe32bf2d7a074af456a7de849c54f89566c4197
Anliks <axel_lines@mail.ru>
2022-01-13 12:52:05 +0300

    java oop template is Complete

--- src/main/java/com/epam/izh/rd/online/repository/SimpleAuthorRepository.java
+++ src/main/java/com/epam/izh/rd/online/repository/SimpleAuthorRepository.java
@@ -1,2 +1,56 @@
-package com.epam.izh.rd.online.repository;public class SimpleAuthorRepository {
+package com.epam.izh.rd.online.repository;
+
+import com.epam.izh.rd.online.entity.Author;
+
+import java.util.Arrays;
+
+public class SimpleAuthorRepository implements AuthorRepository {
+    Author[] authors = new Author[0];
+
+    @Override
+    public boolean save(Author author) {
+        if(findByFullName(author.getName(),author.getLastName()) !=null) {
+            return false;
+        } else {
+            Author[] authorsNew = Arrays.copyOf(authors,authors.length+1);
+            authorsNew[authorsNew.length-1] = author;
+             authors = Arrays.copyOf(authorsNew, authorsNew.length);
+        }
+        return true;
+    }
+
+    @Override
+    public Author findByFullName(String name, String lastname) {
+        for(Author authorFullName : authors) {
+            if(authorFullName.getName().equals(name) && authorFullName.getLastName().equals(lastname)) {
+                return authorFullName;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public boolean remove(Author author) {
+        int count = 0;
+      if(findByFullName(author.getName(), author.getLastName())==null) {
+          return false;
+      } else {
+          Author[] authorsNew = new Author[authors.length - 1];
+          for (int i = 0; i < authors.length; i++) {
+              if(authors[i] != author) {
+                  authorsNew[count] = authors[i];
+                  count++;
+              }
+      }
+          authors = Arrays.copyOf(authorsNew, authorsNew.length);
+          System.out.println(Arrays.toString(authors));
+      }
+
+      return true;
+    }
+
+    @Override
+    public int count() {
+        return authors.length;
+    }
 }
--- src/main/java/com/epam/izh/rd/online/repository/SimpleSchoolBookRepository.java
+++ src/main/java/com/epam/izh/rd/online/repository/SimpleSchoolBookRepository.java
@@ -1,2 +1,66 @@
-package com.epam.izh.rd.online.repository;public class SimpleSchoolBookRepository {
+package com.epam.izh.rd.online.repository;
+
+import com.epam.izh.rd.online.entity.SchoolBook;
+
+import java.util.Arrays;
+
+public class SimpleSchoolBookRepository implements BookRepository<SchoolBook> {
+    SchoolBook[] schoolBooks = new SchoolBook[0];
+
+    @Override
+    public boolean save(SchoolBook book) {
+        SchoolBook[] newSchoolBook = Arrays.copyOf(schoolBooks, schoolBooks.length+1);
+        newSchoolBook[newSchoolBook.length-1] = book;
+        schoolBooks = Arrays.copyOf(newSchoolBook, newSchoolBook.length);
+        for(SchoolBook sb : schoolBooks){
+            if(sb.equals(book)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    @Override
+    public SchoolBook[] findByName(String name) {
+        int plus = 0;
+        SchoolBook[] newSchoolBooks = new SchoolBook[schoolBooks.length];
+        for (int i = 0; i < schoolBooks.length; i++) {
+            if(schoolBooks[i].getName().equals(name)) {
+                newSchoolBooks[plus] = schoolBooks[i];
+                plus++;
+            }
+        }
+        return Arrays.copyOf(newSchoolBooks,plus);
+    }
+
+    @Override
+    public boolean removeByName(String name) {
+        int select = 0;
+        int count = 0;
+        if(findByName(name) == null) {
+            return false;
+        } else {
+            for (SchoolBook counts : schoolBooks) {
+                if (counts.getName().equals(name)) {
+                    count++;
+                }
+            }
+            SchoolBook[] newSchoolBooks = new SchoolBook[schoolBooks.length - count];
+            for (SchoolBook sc : schoolBooks) {
+                if (sc.getName() != name) {
+                    newSchoolBooks[select] = sc;
+                    select++;
+                }
+            }
+            schoolBooks = Arrays.copyOf(newSchoolBooks,newSchoolBooks.length);
+
+            return true;
+        }
+    }
+
+    @Override
+    public int count() {
+        return schoolBooks.length;
+    }
 }
